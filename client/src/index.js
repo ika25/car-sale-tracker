d86826253d104baa68f3840cfd6ca905e8cb1a26
ika25 <lomiika1990@yahoo.com>
2019-11-30 21:34:46 +0000

    created index.js,this will contain main render

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// This is entry point of react app its going contain main render 
+
+// importing react from react this will les us use JSX
+import React from 'react';
+//this will allows us to rener 
+import ReactDom from 'react-dom';
+import App from './App';
+
+ReactDom.render(<App/>,document.getElementById('root'));
