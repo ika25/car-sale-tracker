e60ae268f308f6feb22138e7dc072b5e615c557f
ika25 <lomiika1990@yahoo.com>
2019-12-01 14:42:48 +0000

    editted CarsTable.js

--- client/src/components/CarsTable.js
+++ client/src/components/CarsTable.js
@@ -14,24 +14,12 @@ const CarsTable = (props)=>{
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+  {props.car.map(Cars=>{
+                   return <CarsTableRow key={cars._id}
+                    cars={cars}
+                    deleteHandler={props.deleteHandler}
+                    showEditForm={props.showEditForm}/>
+               })}
   </tbody>
 </table>
        
